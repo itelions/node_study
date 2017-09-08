0d4c6a18ab8455cc10b6af39daa135616a15c4ac
shiWei <694667514@qq.com>
2017-09-08 11:40:11 +0800

    1.添加puppeteer模块

--- routes/puppeteer.js
+++ routes/puppeteer.js
@@ -10,46 +10,46 @@ router.get('/', function(req, res, next) {
 	})
 	puppeteer.launch()
 	.then(_borwser => {
-			borwser = _borwser;
-			return borwser.newPage();
+		borwser = _borwser;
+		return borwser.newPage();
+	})
+	.then(_page => {
+		page = _page;
+		page.setViewport({
+			width: 1280,
+			height: 720
 		})
-		.then(_page => {
-			page = _page;
-			page.setViewport({
-				width: 1280,
-				height: 720
-			})
-			return page.goto(req.query.target);
+		return page.goto(req.query.target);
+	})
+	.then(_ => {
+		return page.waitFor(2000)
+	})
+	// 保存pdf和网页快照
+	// .then(_ => {
+	// 	var randomName = parseInt(Math.random() * 10000) + new Date().getTime();
+	// 	return Promise.all([
+	// 		page.screenshot({ path: './public/screenshot/' + randomName + '.png' }),
+	// 		page.pdf({ path: './public/pdf/' + randomName + '.pdf', format: 'A4' })
+	// 	])
+	// })
+	.then(_ => {
+		return page.content()
+	})
+	.then(_text=>{
+		res.send({
+			msg: 'ok',
+			text:_text,
+			code: 200
 		})
-		.then(_ => {
-			return page.waitFor(2000)
+		borwser.close();	
+	})
+	.catch(_err => {
+		console.log(_err)
+		res.send({
+			msg: 'error',
+			code: 200
 		})
-		// 保存pdf和网页快照
-		// .then(_ => {
-		// 	var randomName = parseInt(Math.random() * 10000) + new Date().getTime();
-		// 	return Promise.all([
-		// 		page.screenshot({ path: './public/screenshot/' + randomName + '.png' }),
-		// 		page.pdf({ path: './public/pdf/' + randomName + '.pdf', format: 'A4' })
-		// 	])
-		// })
-		.then(_ => {
-			return page.content()
-		})
-		.then(_text=>{
-			res.send({
-				msg: 'ok',
-				text:_text,
-				code: 200
-			})
-			borwser.close();	
-		})
-		.catch(_err => {
-			console.log(_err)
-			res.send({
-				msg: 'error',
-				code: 200
-			})
-		})
+	})
 })
 
 module.exports = router;
